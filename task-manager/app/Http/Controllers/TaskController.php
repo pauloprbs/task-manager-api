98d0488cfa6778a5b7c114a16b84bf451b68959d
pauloprbs <paulorenato_bs@live.com>
2024-12-06 15:50:52 -0300

    feat: api básica criada, mas não está retornando as requisições...

---
+++ task-manager/app/Http/Controllers/TaskController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    public function index()
+    {
+        return Task::all();
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'is_completed' => 'boolean',
+        ]);
+
+        return Task::create($request->all());
+    }
+
+    public function show(Task $task)
+    {
+        return $task;
+    }
+
+    public function update(Request $request, Task $task)
+    {
+        $request->validate([
+            'title' => 'string|max:255',
+            'description' => 'nullable|string',
+            'is_completed' => 'boolean',
+        ]);
+
+        $task->update($request->all());
+
+        return $task;
+    }
+
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return response(null, 204);
+    }
+}
---
+++ task-manager/routes/api.php
@@ -0,0 +1,5 @@
+<?php
+
+use App\Http\Controllers\TaskController;
+
+Route::apiResource('tasks', TaskController::class);
